af960fd52a797533b84a8d0ddeae99a8a0054541
LengSword <1030588973@qq.com>
2018-09-05 20:48:35 +0800

    Temp test new online code runner

--- myblog/vendor/extension/load-specials-system/main.js
+++ myblog/vendor/extension/load-specials-system/main.js
@@ -1,1 +1,52 @@
-!function(e){"use strict";function r(e){var r=document.createElement("a");r.href=e;var t=function(e){if(!e)return{};var r={};e=e.slice(1).split("&");for(var t,a=0;a<e.length;a++){t=e[a].split("=");var n=t[0],i=t[1];/\[\]$/.test(n)?(r[n]=r[n]||[],r[n].push(i)):r[n]=i}return r};return{protocol:r.protocol,host:r.host,hostname:r.hostname,pathname:r.pathname,search:t(r.search),hash:r.hash}}e(window).on("loaded-main-page",function(){e("a").on("click",function(){var t=r(this.href),a=t.search.p||"";if("schulte_grid.md"==a){var n='<iframe src="./p/specials/schulte_grid.html" width="100%" height="100%" scrolling="no" frameborder="0"></iframe>';e("#main-page").append(n)}if("online_execute.md"==a){var n='<iframe src="https://tool.lu/coderunner/embed/5bt.html" width="100%" height="100%" frameborder="0"></iframe>';e("#main-page").append(n)}})})}(Zepto);
+! function (e) {
+    "use strict";
+    function urlparser(url) {
+      var a = document.createElement('a');
+      a.href = url;
+      
+      var search = function(search) {
+        if(!search) return {};
+        
+        var ret = {};
+        search = search.slice(1).split('&');
+        for(var i = 0, arr; i < search.length; i++) {
+          arr = search[i].split('=');
+          var key = arr[0], value = arr[1];
+          if(/\[\]$/.test(key)) {
+            ret[key] = ret[key] || [];
+            ret[key].push(value);
+          } else {
+            ret[key] = value;
+          }
+        }
+        return ret;
+      };
+      
+      return {
+        protocol: a.protocol,
+        host: a.host,
+        hostname: a.hostname,
+        pathname: a.pathname,
+        search: search(a.search),
+        hash: a.hash
+      }
+    };
+
+    e(window).on("loaded-main-page", function() {
+        e('a').on("click", function() {
+            var urlobject = urlparser(this.href);
+            var cur_md_path = urlobject.search['p'] || '';
+            
+            if(cur_md_path == "schulte_grid.md") { //Game 0x01 - Schulte Grid
+                var htmlCodes = '<iframe src="./p/specials/schulte_grid.html" width="100%" height="100%" scrolling="no" frameborder="0"></iframe>';
+                e("#main-page").append(htmlCodes);
+            }
+            if(cur_md_path == "online_execute.md") { //Tool 0x02 - Online Code Runner
+                var htmlCodes = '<iframe src="http://www.runoob.com/try/runcode.php?filename=HelloWorld&type=python3" width="100%" height="100%" frameborder="0"></iframe>';
+                e("#main-page").append(htmlCodes);
+            }
+        
+        });
+    });
+    
+}(Zepto);
