c910cc06ed2e91c3421041524e79b4b50b6df1be
LengSword <1030588973@qq.com>
2018-11-14 00:58:46 +0800

    ext & core Optimized 2x

---
+++ myblog/vendor/extension/content-fadeIn/main.js
@@ -0,0 +1,1 @@
+!function(o){"use strict";o(window).on("loaded-main-page",function(){o(".toggle").css("background-image","url('img/toc.png')"),o(".toTop").css("background-image","url('img/up.png')")})}(Zepto);
