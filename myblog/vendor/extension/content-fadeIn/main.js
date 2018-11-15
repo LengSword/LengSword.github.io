07d811ff55cad5bd4c7d8de97ce499ce47b4aa79
LengSword <1030588973@qq.com>
2018-11-16 00:28:41 +0800

    add bottom & core optimized

--- myblog/vendor/extension/content-fadeIn/main.js
+++ myblog/vendor/extension/content-fadeIn/main.js
@@ -1,1 +1,1 @@
-!function(t){"use strict";t(window).on("loaded-main-page",function(){t(".toggle").attr("src","img/toc.png"),t(".toTop").attr("src","img/up.png")})}(Zepto);
+!function(t){"use strict";t(window).on("loaded-main-page",function(){t(".toggle").attr("src","img/toc.png"),t(".toTop").attr("src","img/up.png"),t(".toBottom").attr("src","img/down.png")})}(Zepto);
